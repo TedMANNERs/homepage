31e0f339b07a5f5b1528023d5a83b2d986c71020
TedMANNERs <mannersofted@gmail.com>
2018-04-09 22:40:02 +0200

    Use environment variable for port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.use(express.static("public"));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
 
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
