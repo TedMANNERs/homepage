68edfca686a41a098b2166a0b1f4ae141a0bd69f
TedMANNERs <mannersofted@gmail.com>
2018-04-11 00:52:21 +0200

    Add style switcher

---
+++ public/js/scripts.js
@@ -0,0 +1,51 @@
+function switchColorMode() {
+    var activeStyle = getActiveStyle();
+    if(activeStyle === "default") 
+        setActiveStyle("alternate");
+    else
+        setActiveStyle("default");    
+}
+
+function getStyleCookie() {
+    var cookiePrefix = "style=";
+    var cookies = document.cookie.split(";");
+    cookies.forEach(c => {
+        if(c.indexOf(cookiePrefix == 0))
+            return c.substring(cookiePrefix.length, c.length);
+    });
+}
+
+function getActiveStyle() {
+    var i, a;
+    var links = document.getElementsByTagName("link");
+    for(i=0; (a = links[i]); i++) {
+        var titleAttribute = a.getAttribute("title");
+        if(a.getAttribute("rel").indexOf("style") != -1 && titleAttribute && !a.disabled) 
+            return titleAttribute;
+    }
+    return null;
+}
+
+function setActiveStyle(title) {
+    var i, a;
+    var links = document.getElementsByTagName("link");
+    for(i=0; (a = links[i]); i++) {
+        var titleAttribute = a.getAttribute("title");
+        if(a.getAttribute("rel").indexOf("style") != -1 && titleAttribute) {
+            a.disabled = true;
+            if(titleAttribute == title) 
+                a.disabled = false;
+        }
+    }
+}
+
+window.onload = function(e) {
+    var cookie = getStyleCookie();
+    var title = cookie ? cookie : "default";
+    setActiveStyle(title);
+}
+  
+window.onunload = function(e) {
+    var title = getActiveStyleSheet();
+  document.cookie = "style=" + title + ";";
+}
